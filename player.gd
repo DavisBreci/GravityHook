b9970384a9c8e39e0a748b4ca155706804029f54
DavisBreci <davisbreci@outlook.com>
2026-09-15 00:20:37 -0400

    fixed major movement bugs, corner bug still remains

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ var fall_speed
 var gravity
 var gravity_direction
 var collision_vector
+var direction = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fall_speed = 0
@@ -10,42 +11,45 @@ func _ready() -> void:
 	gravity = 10
 	gravity_direction = Vector2(0,1)
 
-var was_on_floor = false
-var was_on_ceiling = false
-var was_on_wall = false
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	var is_airborne = (not is_on_floor() and gravity_direction == Vector2(0,1)) or (not is_on_ceiling() and gravity_direction == Vector2(0,-1)) or (not is_on_wall() and gravity_direction.x != 0)
 	collision_vector = Vector2.ZERO
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var normal = collision.get_normal()
 		collision_vector += normal
-	if collision_vector.x != 0 and not was_on_wall:
-		var cos_theta = (Vector2((-1) * collision_vector.x,0)).dot(get_real_velocity().normalized())
-		if cos_theta > 0.8:
+	var cos_theta
+	if collision_vector.x != 0:
+		cos_theta = (Vector2((-1) * collision_vector.x, 0)).dot(get_real_velocity().normalized())
+		if cos_theta > 0.9:
 			gravity_direction = Vector2((-1 * collision_vector.x), 0)
-	if collision_vector.y > 0 and not was_on_ceiling:
-		gravity_direction = Vector2(0,-1)
-	elif collision_vector.y < 0 and not was_on_floor:
-		gravity_direction = Vector2(0,1)
-	was_on_floor = is_on_floor()
-	was_on_ceiling = is_on_ceiling()
-	was_on_wall = is_on_wall()
+	if collision_vector.y > 0:
+		cos_theta = (Vector2(0,-1)).dot(get_real_velocity().normalized())
+		if cos_theta > 0.9:
+			gravity_direction = Vector2(0,-1)
+	if collision_vector.y < 0:
+		cos_theta = (Vector2(0,1)).dot(get_real_velocity().normalized())
+		if cos_theta > 0.9:
+			gravity_direction = Vector2(0,1)
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		velocity.x += -250
+		velocity.x += -400
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 250
+		velocity.x += 400
 	if Input.is_action_pressed("move_up"):
-		velocity.y += -250
+		velocity.y += -400
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 250
+		velocity.y += 400
+	if Input.is_action_pressed("dash"):
+		velocity *= 1.8
 	velocity -= velocity * abs(gravity_direction)
-	var is_airborne = not (is_on_floor() or is_on_ceiling() or is_on_wall())
 	if not is_airborne:
-		fall_speed = 10
+		fall_speed = 200
 		if Input.is_action_just_pressed("jump"):
 			gravity_direction *= -1
+			velocity += gravity_direction * fall_speed
 	if is_airborne:
-		fall_speed += (gravity * 0.2) if Input.is_action_pressed("jump") else gravity
-	velocity += gravity_direction * fall_speed
+		fall_speed += gravity
+		velocity += gravity_direction * fall_speed
+	direction = velocity.normalized()
