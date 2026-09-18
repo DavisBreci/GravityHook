9136bbe4cacce60665f3818585ad9a0019fea823
DavisBreci <davisbreci@outlook.com>
2026-09-18 10:37:14 -0400

    refactored user input to run every frame instead of every physics step. bug found when holding diagonally into corner, although it is related to the gravity switch pause, not the physics handling

--- player.gd
+++ player.gd
@@ -1,16 +1,36 @@
 extends CharacterBody2D
 signal gravity_change
-var fall_speed
+var fall_speed = 0
 const GRAVITY = 16
-var gravity_direction
+var gravity_direction = Vector2(0,1)
 var is_airborne = true
 const SPEED = 400
+var directional_input = Vector2.ZERO
+var button_input = [false, false]
+enum buttons {
+	JUMP = 0,
+	DASH = 1
+}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fall_speed = 0
 	position.x = 100
-	gravity_direction = Vector2(0,1)
-	velocity = Vector2(0,0)
+	velocity = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	directional_input = Vector2.ZERO
+	button_input = [false, false]
+	if Input.is_action_pressed("move_left"):
+		directional_input += Vector2(-1,0)
+	if Input.is_action_pressed("move_right"):
+		directional_input += Vector2(1,0)
+	if Input.is_action_pressed("move_up"):
+		directional_input += Vector2(0,-1)
+	if Input.is_action_pressed("move_down"):
+		directional_input += Vector2(0,1)
+	if Input.is_action_pressed("jump"):
+		button_input[buttons.JUMP] = true
+	if Input.is_action_pressed("dash"):
+		button_input[buttons.DASH] = true
 
 func set_gravity(new_direction: Vector2) -> void:
 	if gravity_direction != new_direction:
@@ -34,18 +54,8 @@ func _physics_process(delta: float) -> void:
 			is_airborne = false
 			set_gravity((-1)*normal)
 	
-	velocity = Vector2.ZERO
-	var input = Vector2.ZERO
-	if Input.is_action_pressed("move_left"):
-		input += Vector2(-1,0)
-	if Input.is_action_pressed("move_right"):
-		input += Vector2(1,0)
-	if Input.is_action_pressed("move_up"):
-		input += Vector2(0,-1)
-	if Input.is_action_pressed("move_down"):
-		input += Vector2(0,1)
-	velocity = input * SPEED
-	if Input.is_action_pressed("dash"):
+	velocity = directional_input * SPEED
+	if button_input[buttons.DASH]:
 		velocity *= 1.8
 	if gravity_direction.x != 0:
 		velocity.x = 0
@@ -53,7 +63,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = 0
 	if not is_airborne:
 		fall_speed = 200
-		if Input.is_action_just_pressed("jump"):
+		if button_input[buttons.JUMP]:
 			is_airborne = true
 			set_gravity((-1)*gravity_direction)
 			velocity += gravity_direction * fall_speed
