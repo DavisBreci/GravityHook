6dbbb34ca94350c717a29f84a4e168aa01a6c394
DavisBreci <davisbreci@outlook.com>
2026-09-18 02:00:13 -0400

    major overhaul to movement mechanics, preliminary textures to be replaced

--- level.gd
+++ level.gd
@@ -3,9 +3,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Sprite2D/ArrowTexture.modulate.a = 0.5
+	$Sprite2D/ArrowTexture.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$ArrowTexture.rotation = atan2($Player.gravity_direction.y, $Player.gravity_direction.x) + PI/2
+	pass
+
+func _on_player_gravity_change(new_gravity: Vector2) -> void:
+	get_tree().paused = true
+	$Player/Sprite2D.rotation = atan2(new_gravity.y, new_gravity.x) - PI/2
+	$Sprite2D/ArrowTexture.rotation = atan2(new_gravity.y, new_gravity.x) + PI/2
+	$Sprite2D/ArrowTexture.show()
+	await get_tree().create_timer(0.1).timeout
+	$Sprite2D/ArrowTexture.hide()
+	get_tree().paused = false
+	
--- player.gd
+++ player.gd
@@ -1,55 +1,62 @@
 extends CharacterBody2D
+signal gravity_change
 var fall_speed
-var gravity
+const GRAVITY = 16
 var gravity_direction
-var collision_vector
-var direction = Vector2.ZERO
+var is_airborne = true
+const SPEED = 400
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fall_speed = 0
 	position.x = 100
-	gravity = 10
 	gravity_direction = Vector2(0,1)
+	velocity = Vector2(0,0)
+
+func set_gravity(new_direction: Vector2) -> void:
+	if gravity_direction != new_direction:
+		gravity_change.emit(new_direction)
+		gravity_direction = new_direction
 
 func _physics_process(delta: float) -> void:
-	move_and_slide()
-	var is_airborne = (not is_on_floor() and gravity_direction == Vector2(0,1)) or (not is_on_ceiling() and gravity_direction == Vector2(0,-1)) or (not is_on_wall() and gravity_direction.x != 0)
-	collision_vector = Vector2.ZERO
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var normal = collision.get_normal()
-		collision_vector += normal
-	var cos_theta
-	if collision_vector.x != 0:
-		cos_theta = (Vector2((-1) * collision_vector.x, 0)).dot(get_real_velocity().normalized())
-		if cos_theta > 0.9:
-			gravity_direction = Vector2((-1 * collision_vector.x), 0)
-	if collision_vector.y > 0:
-		cos_theta = (Vector2(0,-1)).dot(get_real_velocity().normalized())
-		if cos_theta > 0.9:
-			gravity_direction = Vector2(0,-1)
-	if collision_vector.y < 0:
-		cos_theta = (Vector2(0,1)).dot(get_real_velocity().normalized())
-		if cos_theta > 0.9:
-			gravity_direction = Vector2(0,1)
+	var collision = move_and_collide(velocity*delta, false, 0.2, true)
+	var normal = Vector2.ZERO
+	if collision:
+		normal = collision.get_normal()
+		if normal.x != 0:
+			position.y += collision.get_remainder().y
+		else:
+			position.x += collision.get_remainder().x
+	
+	if normal == Vector2.ZERO:
+		is_airborne = true
+	else:
+		if normal + gravity_direction == Vector2.ZERO or velocity.normalized().dot(normal) < -0.95:
+			is_airborne = false
+			set_gravity((-1)*normal)
+	
 	velocity = Vector2.ZERO
+	var input = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		velocity.x += -400
+		input += Vector2(-1,0)
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 400
+		input += Vector2(1,0)
 	if Input.is_action_pressed("move_up"):
-		velocity.y += -400
+		input += Vector2(0,-1)
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 400
+		input += Vector2(0,1)
+	velocity = input * SPEED
 	if Input.is_action_pressed("dash"):
 		velocity *= 1.8
-	velocity -= velocity * abs(gravity_direction)
+	if gravity_direction.x != 0:
+		velocity.x = 0
+	elif gravity_direction.y != 0:
+		velocity.y = 0
 	if not is_airborne:
 		fall_speed = 200
 		if Input.is_action_just_pressed("jump"):
-			gravity_direction *= -1
+			is_airborne = true
+			set_gravity((-1)*gravity_direction)
 			velocity += gravity_direction * fall_speed
 	if is_airborne:
-		fall_speed += gravity
+		fall_speed += GRAVITY
 		velocity += gravity_direction * fall_speed
-	direction = velocity.normalized()
